febe8945c551cb5e336d4df7ed69bf7b0261e9f9
bolhoso <142200+bolhoso@users.noreply.github.com>
2020-12-05 10:24:29 -0800

    random pieces creation

--- tetris.c
+++ tetris.c
@@ -13,12 +13,14 @@
 #define SCREEN_W 640
 #define SCREEN_H 480
 
-#define FIELD_ROWS 22
+#define FIELD_ROWS 20
 #define FIELD_COLS 10
 
-#define PIECE_SIZE_PX 20
+#define TILE_SIZE_PX 20
 
-#define COLOR_BG   al_map_rgb_f(0, 0, 0)
+#define COLOR_BG      al_map_rgb_f(0, 0, 0)
+#define tile_center() (FIELD_COLS / 2)
+
 
 #define FPS 30
 #define GAME_TIMER_SPEED ((float)1/FPS)
@@ -27,13 +29,56 @@
 #define KEY_SEEN     1
 #define KEY_RELEASED 2
 
+#define TILES_BY_PIECE 4
+
 typedef struct object {
-	// this is discrete x,y in the field, not in the screen, relative to piece top
+	// this is discrete x,y in the field, not in the screen, relative to tile top
 	int col, row;
 
 	ALLEGRO_COLOR color;
-	short type;
 } tile;
+
+enum piece_type {
+
+	//  XX  XX
+	// XX    XX
+	PC_Z = 0,
+	PC_INV_Z,
+
+	//  X
+	// XXX
+	PC_MIDDLE_FINGER,
+
+	// X      X
+	// XXX  XXX
+	PC_L,
+	PC_INV_L,
+
+	// XX
+	// XX
+	PC_SQUARE,
+
+	// XXXX
+	PC_LONG,
+
+	PC_N,
+};
+
+enum direction {
+	D_NORTH = 0,
+	D_EAST,
+	D_SOUTH,
+	D_WEST,
+	D_N
+};
+
+
+typedef struct piece {
+	tile *tiles[TILES_BY_PIECE];
+
+	short direction;
+	enum piece_type type;
+} piece;
 
 typedef struct game_data {
 	tile *field[FIELD_ROWS][FIELD_COLS];
@@ -98,13 +143,81 @@ void init_allegro() {
 	memset(key, 0, sizeof(key));
 }
 
-tile *create_piece() {
+// TODO: just to refactor and reduce impact...
+tile *create_tile_ref(int row, int col) {
 	tile *new = (tile *) malloc(sizeof (tile));
 	new->color = al_map_rgb_f(0.5, 0.5, 0.8);
-	new->type = 0; // TODO: random type
-	new->col = piece_center(new);
-	new->row = 0;
+	new->col = col;
+	new->row = row;
 	return new;
+}
+
+tile *create_tile() {
+	return create_tile_ref(0, tile_center());
+}
+
+piece *create_piece(short type, int top_r, int top_c) {
+	piece *p = (piece *) malloc(sizeof(piece));
+	p->direction = D_NORTH;
+	p->type = type;
+
+	int inv = (type == PC_INV_Z || type == PC_INV_L) ? -1 : 1;
+
+	switch(type) {
+	// 0       0
+	// 123   321
+	case PC_INV_L:
+	case PC_L:
+		p->tiles[0] = create_tile_ref(top_r, top_c);
+		p->tiles[1] = create_tile_ref(top_r + 1, top_c);
+		p->tiles[2] = create_tile_ref(top_r + 1, top_c + inv * 1);
+		p->tiles[3] = create_tile_ref(top_r + 1, top_c + inv * 2);
+		break;
+
+	//  0
+	// 123
+	case PC_MIDDLE_FINGER:
+		p->tiles[0] = create_tile_ref(top_r, top_c);
+		p->tiles[1] = create_tile_ref(top_r + 1, top_c - 1);
+		p->tiles[2] = create_tile_ref(top_r + 1, top_c);
+		p->tiles[3] = create_tile_ref(top_r + 1, top_c + 1);
+		break;
+
+	//  01    10
+    // 32      23
+	case PC_INV_Z:
+	case PC_Z:
+		p->tiles[0] = create_tile_ref(top_r, top_c);
+		p->tiles[1] = create_tile_ref(top_r, top_c + inv * 1);
+		p->tiles[2] = create_tile_ref(top_r + 1, top_c);
+		p->tiles[3] = create_tile_ref(top_r + 1, top_c - inv * 1);
+		break;
+
+	case PC_SQUARE:
+		p->tiles[0] = create_tile_ref(top_r, top_c);
+		p->tiles[1] = create_tile_ref(top_r, top_c + 1);
+		p->tiles[2] = create_tile_ref(top_r + 1, top_c);
+		p->tiles[3] = create_tile_ref(top_r + 1, top_c + 1);
+		break;
+
+	case PC_LONG:
+		p->tiles[0] = create_tile_ref(top_r, top_c);
+		p->tiles[1] = create_tile_ref(top_r + 1, top_c);
+		p->tiles[2] = create_tile_ref(top_r + 2, top_c);
+		p->tiles[3] = create_tile_ref(top_r + 3, top_c);
+		break;
+
+	default:
+		printf ("unknown piece!!");
+		exit(-1);
+
+	}
+
+	return p;
+}
+
+piece *create_random_piece() {
+	return create_piece(rand() % PC_N, 0, tile_center());
 }
 
 void init_objects(game_data *data) {
@@ -115,35 +228,44 @@ void init_objects(game_data *data) {
 			data->field[i][j] = NULL;
 		}
 	}
-	data->falling = create_piece();
+	data->falling = create_tile();
 
 	data->points = 0;
 	data->speed = INITIAL_SPEED;
 }
 
 int to_scrx(int field_x) {
-	return field_x * PIECE_SIZE_PX;
+	return field_x * TILE_SIZE_PX;
 }
 
 int to_scry(int field_y) {
-	return field_y * PIECE_SIZE_PX;
+	return field_y * TILE_SIZE_PX;
 }
 
-void draw_piece(tile *p) {
+void draw_tile(tile *p) {
 	if (!p)
 		return;
 
 	int x = to_scrx(p->col);
 	int y = to_scry(p->row);
 
-	al_draw_filled_rectangle(x, y, x + PIECE_SIZE_PX, y + PIECE_SIZE_PX, p->color);
+	al_draw_filled_rectangle(x, y, x + TILE_SIZE_PX, y + TILE_SIZE_PX, p->color);
+}
+
+void draw_piece(piece *p) {
+	if (!p)
+		return;
+
+	for (int i = 0; i < TILES_BY_PIECE; i++) {
+		draw_tile(p->tiles[i]);
+	}
 }
 
 void draw_field(game_data *g) {
 	for (int i = 0; i < g->field_rows; i++) {
 		for (int j = 0; j < g->field_cols; j++) {
 			if (g->field[i][j] != NULL) {
-				draw_piece(g->field[i][j]);
+				draw_tile(g->field[i][j]);
 			}
 		}
 	}
@@ -153,15 +275,11 @@ void draw_screen(game_data *g) {
 	al_clear_to_color(COLOR_BG);
 	al_draw_text(font, al_map_rgb_f(1, 1, 1), SCREEN_W / 2, 0, 0, "GAME!"); // TODO: how to center text?
 
-	draw_piece(g->falling);
+//	TODO: testing draw_piece(create_random_piece());
+	draw_tile(g->falling);
 	draw_field(g);
 
 	al_flip_display();
-}
-
-int piece_center (tile *p) {
-	// for now, just return field center
-	return FIELD_COLS / 2;
 }
 
 bool can_move(game_data *g, tile *free, int dcol, int drow) {
@@ -175,16 +293,18 @@ bool can_move(game_data *g, tile *free, int dcol, int drow) {
 	return within_field && !free_below;
 }
 
-void drop_pieces(game_data *game, int row) {
+void drop_tiles(game_data *game, int row) {
 	for (int r = row; r > 0; r--) {
 		for (int c = 0; c < game->field_cols; c++) {
-			tile *upper_piece = game->field[r-1][c];
-			game->field[r][c] = upper_piece;
+			tile *upper_tile = game->field[r-1][c];
+			game->field[r][c] = upper_tile;
 
-			if (upper_piece) {
+			if (upper_tile) {
 				game->field[r][c]->col = c;
 				game->field[r][c]->row = r;
 			}
+
+			// TODO: call free()
 		}
 	}
 }
@@ -196,7 +316,7 @@ bool disappear_line(game_data *game, int row) {
 	}
 
 	if (row_complete) {
-		drop_pieces(game, row);
+		drop_tiles(game, row);
 	}
 
 	return row_complete;
@@ -205,7 +325,7 @@ bool disappear_line(game_data *game, int row) {
 bool game_logic(game_data *game) {
 	if (game->falling == NULL) {
 		exit(1); // should never reach here :/
-		game->falling = create_piece();
+		game->falling = create_tile();
 	}
 
 	// only move pieces every FPS ticks
@@ -225,7 +345,7 @@ bool game_logic(game_data *game) {
 			game->falling->row++;
 		} else {
 			game->field[py][px] = game->falling;
-			game->falling = create_piece();
+			game->falling = create_tile();
 
 			disappear_line(game, py);
 		}
@@ -274,7 +394,7 @@ int main() {
 
 	// TODO: debug
 	for (int i = 0; i < game_data.field_cols - 1; i++) {
-		tile *p = create_piece();
+		tile *p = create_tile();
 		p->row = game_data.field_rows - 1;
 		p->col = i;
 		game_data.field[game_data.field_rows - 1][i] = p;
